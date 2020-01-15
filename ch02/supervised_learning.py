9bd1a368566843c91fdd18c88a74ac5a72060bfd
naokifile <startri.sea@gmail.com>
2020-01-15 17:36:00 +0900

    p81

--- ch02/supervised_learning.py
+++ ch02/supervised_learning.py
@@ -6,6 +6,7 @@ import pandas as pd
 import mglearn
 from IPython.display import display
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 # データセットの生成
 #X, y = mglearn.datasets.make_forge()
@@ -217,5 +218,130 @@ plt.show()
 
 """
 
-mglearn.plots.plot_ridge_n_samples()
+
+#mglearn.plots.plot_ridge_n_samples()
+#plt.show()
+
+
+# lasso
+"""
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+lasso = Lasso().fit(X_train, y_train)
+#print("Training set score: {:.2f}".format(lasso.score(X_train, y_train)))
+#print("Test set score: {:.2f}".format(lasso.score(X_test, y_test)))
+#print("Number of features used: {}".format(np.sum(lasso.coef_ != 0)))
+
+# "max_iter"の値を増やしている
+# こうしておかないとモデルが、"max_iter"を増やすように警告を発する
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+#print("Training set score: {:.2f}".format(lasso001.score(X_train, y_train)))
+#print("Test set score: {:.2f}".format(lasso001.score(X_test, y_test)))
+#print("Number of features used: {}".format(np.sum(lasso001.coef_ != 0)))
+
+
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+#print("Training set score: {:.2f}".format(lasso00001.score(X_train, y_train)))
+#print("Test set score: {:.2f}".format(lasso00001.score(X_test, y_test)))
+#print("Number of features used: {}".format(np.sum(lasso00001.coef_ != 0)))
+
+plt.plot(lasso.coef_, "s", label="Lasso alpha=1")
+plt.plot(lasso001.coef_, "^", label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, "v", label="Lasso alpha=0.00001")
+
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25,25)
+plt.xlabel("Coefficient index")
+plt.ylabel("Coefficient magnitude")
+plt.show()
+"""
+
+
+# p74
+"""
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+X, y = mglearn.datasets.make_forge()
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5,
+                                    ax=ax, alpha=.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+axes[0].legend()
+
+plt.show()
+
+"""
+
+#mglearn.plots.plot_linear_svc_regularization()
+#plt.show()
+
+# p77
+#from sklearn.linear_model import LogisticRegression
+#from sklearn.datasets import load_breast_cancer
+#cancer = load_breast_cancer()
+#X_train, X_test, y_train, y_test = train_test_split(
+#    cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+
+"""
+logreg = LogisticRegression().fit(X_train, y_train)
+#print("Training set score: {:.3f}".format(logreg.score(X_train, y_train)))
+#print("Test set score: {:.3f}".format(logreg.score(X_test, y_test)))
+
+logreg100 = LogisticRegression(C=100).fit(X_train, y_train)
+#print("Training set score: {:.3f}".format(logreg100.score(X_train, y_train)))
+#print("Test set score: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+logreg001 = LogisticRegression(C=0.01).fit(X_train, y_train)
+#print("Training set score: {:.3f}".format(logreg001.score(X_train, y_train)))
+#print("Test set score: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+plt.plot(logreg.coef_.T, "o", label="C=1")
+plt.plot(logreg100.coef_.T, "^", label="C=100")
+plt.plot(logreg001.coef_.T, "v", label="C=0.001")
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+#plt.hline(0, 0, cancer.data.shape[1])
+plt.ylim(-5, 5)
+plt.xlabel("Feature")
+plt.ylabel("Coefficient magnitude")
+plt.legend()
+plt.show()
+
+for C, marker in zip([0.001, 1, 100], ["o", "^", "v"]):
+    lr_l1 = LogisticRegression(C=C, penalty="l1", solver="liblinear").fit(X_train, y_train)
+    print("Training accuracy of l1 logreg with C={:.3f}: {:.2f}".format(
+        C, lr_l1.score(X_train, y_train)))
+    print("Test accuracy of l1 logreg with C={:.3f}: {:.2f}".format(
+        C, lr_l1.score(X_test, y_test)))
+    plt.plot(lr_l1.coef_.T, marker, label="C={:.3f}".format(C))
+
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+#plt.hline(0, 0, cancer.data.shape[1])
+plt.xlabel("Feature")
+plt.ylabel("Coefficient magnitude")
+
+plt.ylim(-5, 5)
+plt.legend(loc=3)
+plt.show()
+
+"""
+
+
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:,0], X[:, 1], y)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(["Class 0", "Class 1", "Class 2"])
 plt.show()
