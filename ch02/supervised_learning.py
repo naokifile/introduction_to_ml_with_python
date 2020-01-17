b66d8b6a9385db2f1ee54ff5f9eef3cb2c5ebb45
naokifile <startri.sea@gmail.com>
2020-01-17 14:12:56 +0900

    p85

--- ch02/supervised_learning.py
+++ ch02/supervised_learning.py
@@ -336,12 +336,54 @@ plt.show()
 
 """
 
-
+"""
+from sklearn.svm import LinearSVC
 from sklearn.datasets import make_blobs
 
 X, y = make_blobs(random_state=42)
-mglearn.discrete_scatter(X[:,0], X[:, 1], y)
+#mglearn.discrete_scatter(X[:,0], X[:, 1], y)
+#plt.xlabel("Feature 0")
+#plt.ylabel("Feature 1")
+#plt.legend(["Class 0", "Class 1", "Class 2"])
+#plt.show()
+
+linear_smv = LinearSVC().fit(X, y)
+print("Coefficient shape: ", linear_smv.coef_.shape)
+print("Intercept shape: ", linear_smv.intercept_.shape)
+
+
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_smv.coef_, linear_smv.intercept_,
+                                  ["b", "r", "g"]):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.ylim(-10, 15)
+plt.xlim(-10, 8)
 plt.xlabel("Feature 0")
 plt.ylabel("Feature 1")
-plt.legend(["Class 0", "Class 1", "Class 2"])
+plt.legend(["Class 0", "Class 1", "Class 2", "Line class 0", "Line class 1",
+            "Line class 2"], loc=(1.01, 0.3))
+
+mglearn.plots.plot_2d_classification(linear_smv, X, fill=True, alpha=.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(linear_smv.coef_, linear_smv.intercept_,
+                                  ["b", "r", "g"]):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.xlabel("Feature 0")
+plt.ylabel("Feature 1")
+plt.legend(["Class 0", "Class 1", "Class 2", "Line class 0", "Line class 1",
+            "Line class 2"], loc=(1.01, 0.3))
 plt.show()
+"""
+
+
+
+
+
+
+
+
+
+
+
